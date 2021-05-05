3ed620069d19346915b0d427f39b20492e144ace
Hardik Pachory <pachory42@gmail.com>
2021-05-05 21:08:35 +0530

    Final_Commit_1

--- mywebapp/urls.py
+++ mywebapp/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('imageupload/', include('ImgUpload.urls')),
+    path('', include('ImgUpload.urls')),
 ]
